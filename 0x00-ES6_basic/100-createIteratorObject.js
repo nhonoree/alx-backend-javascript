36741cbe34590985a27a83bb242cf2b80a5c3d81
nhonoree <honoreniyirema@gmail.com>
2025-04-17 22:56:01 +0000

    updates

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,7 @@
 export default function createIteratorObject(report) {
   const allEmployees = report.allEmployees;
 
-  function* iterate() {
+  function *iterate() {
     for (const dept of Object.values(allEmployees)) {
       for (const employee of dept) {
         yield employee;
