a3c0d4050a49994aa881df57d26b3e11c2dc8cbb
nhonoree <honoreniyirema@gmail.com>
2025-04-23 13:20:57 +0000

    advanced task

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+// Create and export the WeakMap
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const currentCount = weakMap.get(endpoint);
+    if (currentCount >= 4) {
+      throw new Error('Endpoint load is high');
+    }
+    weakMap.set(endpoint, currentCount + 1);
+  }
+}
